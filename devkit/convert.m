04eca1e14f2749abbcaebdffb744ad75cd3b3a3b
Asif Mohaimen <asifmohaimen@student.sust.edu>
2019-05-07 10:59:35 +0600

    converted annotations to csv

---
+++ devkit/convert.m
@@ -0,0 +1,11 @@
+FileData = load('cars_meta.mat');
+T = struct2table(FileData);
+writetable(T,'cars_meta.csv','Delimiter',',');
+
+FileData = load('cars_test_annos.mat');
+T = struct2table(FileData);
+writetable(T,'cars_test_annos.csv','Delimiter',',');
+
+FileData = load('cars_train_annos.mat');
+T = struct2table(FileData);
+writetable(T,'cars_train_annos.csv','Delimiter',',');
