88a7b837d9b500615d7b6663dfb74ab4731d96c2
Asif Mohaimen <asifmohaimen@student.sust.edu>
2019-05-07 11:15:44 +0600

    Updated conversion tech

--- devkit/convert.m
+++ devkit/convert.m
@@ -3,9 +3,9 @@ T = struct2table(FileData);
 writetable(T,'cars_meta.csv','Delimiter',',');
 
 FileData = load('cars_test_annos.mat');
-T = struct2table(FileData);
+T = struct2table(cell2mat(struct2cell(FileData)));
 writetable(T,'cars_test_annos.csv','Delimiter',',');
 
 FileData = load('cars_train_annos.mat');
-T = struct2table(FileData);
+T = struct2table(cell2mat(struct2cell(FileData)));
 writetable(T,'cars_train_annos.csv','Delimiter',',');
