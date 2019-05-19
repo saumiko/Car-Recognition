9cf0f5e217c60ebc7f1c699b6cc7500c1336559c
saumiko <mohaimen@tigerit.com>
2019-05-19 16:04:10 +0600

    Sparse class according to ratio. Ignoring classes with Data less than 40 items.

--- prepare_meta.py
+++ prepare_meta.py
@@ -19,6 +19,12 @@ os.makedirs(output_dir)
 
 class_names = []
 annotations = []
+annotations_by_class = {}
+
+
+def percentage(percent, whole):
+  return int((percent * whole) / 100)
+
 
 # Parse all annotation files and prepare data
 for filename in os.listdir(input_dir):
@@ -47,101 +53,108 @@ for filename in os.listdir(input_dir):
             annotations.append(annotation)
 
 
-# Make sure all classes exist in training set
-def check_all_class_in_training_set():
-    train_annos = annotations[:training]
-    train_classes = []
-    for annotation in train_annos:
-        if annotation['class'] not in train_classes:
-            train_classes.append(annotation['class'])
-    if(len(train_classes) == len(class_names)):
-        return False
-    else:
-        return True
 
 
-while(check_all_class_in_training_set()):
-    shuffle(annotations)
+# prepare annotations by class
+for class_name in class_names:
+    class_annos = []
+    for annotation in annotations:
+        if annotation['class'] == class_names.index(class_name):
+            class_annos.append(annotation)
+    annotations_by_class[class_name] = class_annos
 
 
-# Write full processed meta data in 60, 20, 20 ratio
+
 csv.register_dialect('myDialect',
                          quoting=csv.QUOTE_ALL,
                          skipinitialspace=True)
 
 
-def percentage(percent, whole):
-  return int((percent * whole) / 100)
+def init_prep_data():
+    header = ['bbox_x1', 'bbox_y1', 'bbox_x2', 'bbox_y2', 'class', 'fname']
+    csvdat = [header]
+    dats = ['/cars_train_annos.csv', '/cars_val_annos.csv', '/cars_test_annos.csv']
+    for datfile in dats:
+        with open(output_dir + datfile, 'w') as f:
+            writer = csv.writer(f, dialect='myDialect')
+            for row in csvdat:
+                writer.writerow(row)
+        f.close()
 
 
-# Training
-header = ['bbox_x1', 'bbox_y1', 'bbox_x2', 'bbox_y2', 'class', 'fname']
-csvdat = [header]
-ckpt = 0
-for i in range(ckpt, percentage(training, len(annotations))):
-    row = annotations[i]
-    rowdat = [
-        row['bbox_x1'],
-        row['bbox_y1'],
-        row['bbox_x2'],
-        row['bbox_y2'],
-        row['class'],
-        row['fname']
-    ]
-    csvdat.append(rowdat)
+def prep_data(annotations, ind):
+    # Write full processed meta data in 60, 20, 20 ratio
+    # Training
+    csvdat = []
+    ckpt = 0
+    for i in range(ckpt, percentage(training, len(annotations))):
+        row = annotations[i]
+        rowdat = [
+            row['bbox_x1'],
+            row['bbox_y1'],
+            row['bbox_x2'],
+            row['bbox_y2'],
+            ind,
+            row['fname']
+        ]
+        csvdat.append(rowdat)
 
-with open(output_dir+'/cars_train_annos.csv', 'w') as f:
-    writer = csv.writer(f, dialect='myDialect')
-    for row in csvdat:
-        writer.writerow(row)
-f.close()
+    with open(output_dir + '/cars_train_annos.csv', 'a+') as f:
+        writer = csv.writer(f, dialect='myDialect')
+        for row in csvdat:
+            writer.writerow(row)
+    f.close()
+
+    # Validation
+    csvdat = []
+    ckpt = percentage(training, len(annotations))
+    for i in range(ckpt, ckpt + percentage(validation, len(annotations))):
+        row = annotations[i]
+        rowdat = [
+            row['bbox_x1'],
+            row['bbox_y1'],
+            row['bbox_x2'],
+            row['bbox_y2'],
+            ind,
+            row['fname']
+        ]
+        csvdat.append(rowdat)
+
+    with open(output_dir + '/cars_val_annos.csv', 'a+') as f:
+        writer = csv.writer(f, dialect='myDialect')
+        for row in csvdat:
+            writer.writerow(row)
+    f.close()
+
+    # Test
+    csvdat = []
+    ckpt = ckpt + percentage(validation, len(annotations))
+    for i in range(ckpt, ckpt + percentage(test, len(annotations))):
+        row = annotations[i]
+        rowdat = [
+            row['bbox_x1'],
+            row['bbox_y1'],
+            row['bbox_x2'],
+            row['bbox_y2'],
+            ind,
+            row['fname']
+        ]
+        csvdat.append(rowdat)
+
+    with open(output_dir + '/cars_test_annos.csv', 'a+') as f:
+        writer = csv.writer(f, dialect='myDialect')
+        for row in csvdat:
+            writer.writerow(row)
+    f.close()
 
 
-# Validation
-header = ['bbox_x1', 'bbox_y1', 'bbox_x2', 'bbox_y2', 'class', 'fname']
-csvdat = [header]
-ckpt = percentage(training, len(annotations))
-for i in range(ckpt, ckpt+percentage(validation, len(annotations))):
-    row = annotations[i]
-    rowdat = [
-        row['bbox_x1'],
-        row['bbox_y1'],
-        row['bbox_x2'],
-        row['bbox_y2'],
-        row['class'],
-        row['fname']
-    ]
-    csvdat.append(rowdat)
-
-with open(output_dir + '/cars_val_annos.csv', 'w') as f:
-    writer = csv.writer(f, dialect='myDialect')
-    for row in csvdat:
-        writer.writerow(row)
-f.close()
-
-
-# Test
-header = ['bbox_x1', 'bbox_y1', 'bbox_x2', 'bbox_y2', 'class', 'fname']
-csvdat = [header]
-ckpt = ckpt+percentage(validation, len(annotations))
-for i in range(ckpt, ckpt+percentage(test, len(annotations))):
-    row = annotations[i]
-    rowdat = [
-        row['bbox_x1'],
-        row['bbox_y1'],
-        row['bbox_x2'],
-        row['bbox_y2'],
-        row['class'],
-        row['fname']
-    ]
-    csvdat.append(rowdat)
-
-with open(output_dir + '/cars_test_annos.csv', 'w') as f:
-    writer = csv.writer(f, dialect='myDialect')
-    for row in csvdat:
-        writer.writerow(row)
-f.close()
-
+class_names = []
+init_prep_data()
+for key, value in annotations_by_class.items():
+    if len(value) > 40:
+        class_names.append(key)
+        prep_data(value, class_names.index(key))
+        print(key, len(value))
 
 # Write Meta Data
 header = ['class_id', 'class_name']
